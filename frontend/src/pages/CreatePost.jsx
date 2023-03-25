a8efcef40fadb1d00629282ebf2c959af6dd7b0d
Sonia Abhyankar <abhyankar.sonia@gmail.com>
2023-03-25 01:08:38 +0100

    Add functionality of the form field component

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -36,6 +36,14 @@ const CreatePost = () => {
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Jane Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
             labelName="Prompt"
             type="text"
             name="prompt"
